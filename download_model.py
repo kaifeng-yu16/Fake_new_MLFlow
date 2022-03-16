32b80d96def9de458a88cda4ffc0d7357b61ffb9
kaifeng-yu16 <90477174+kaifeng-yu16@users.noreply.github.com>
2022-03-16 16:09:08 -0400

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -2,5 +2,5 @@ from mlflow.store.artifact.models_artifact_repo import ModelsArtifactRepository
 from mlflow.tracking import MlflowClient
 
 client = MlflowClient()
-my_model = client.download_artifacts("1a3dacf13bbf49e884fa0895cade96b5", path="model")
+my_model = client.download_artifacts("00307ff7a4bc40a8b0db66cf8e375eee", path="model")
 print(f"Placed model in: {my_model}")
